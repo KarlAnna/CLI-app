bcaa2c5b20168398dee65a8cb96ac19c6d1b61a8
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2023-03-03 14:33:07 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,75 +1,53 @@
-const shortid = require("shortid")
-const fs = require("fs").promises
-const path = require("path")
-const contactsPath = path.resolve('./db/contacts.json')
+const shortid = require("shortid");
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.resolve("./db/contacts.json");
 
-async function updateListContacts(newContent) {
-  try {
-    return await fs.writeFile(contactsPath, JSON.stringify(newContent), "utf8")
-  } catch (error) {
-    console.error(error)
+try {
+  async function updateListContacts(newContent) {
+    return await fs.writeFile(contactsPath, JSON.stringify(newContent), "utf8");
   }
-}
 
-async function readContactsFile() {
-  try {
-    return await fs.readFile(contactsPath, 'utf8')
-  } catch (error) {
-    console.error(error)
+  async function readContactsFile() {
+    return await fs.readFile(contactsPath, "utf8");
   }
-}
 
-
-
-async function listContacts() {
-  try {
-    readContactsFile().then(JSON.parse).then(console.table)
-  } catch (error) {
-    console.error(error)
+  async function listContacts() {
+    readContactsFile().then(JSON.parse).then(console.table);
   }
-}
 
-async function getContactById(contactId) {
-  try {
-    const data = await readContactsFile().then(JSON.parse)
-    const result = data.find(item => item.id === contactId)
+  async function getContactById(contactId) {
+    const data = await readContactsFile().then(JSON.parse);
+    const result = data.find((item) => item.id === contactId);
     if (!result) {
-      return null
+      return null;
     }
-    console.table(result)
-  } catch (error) {
-    console.error(error)
+    console.table(result);
   }
-}
 
-async function removeContact(contactId) {
-  try {
-    const data = await readContactsFile().then(JSON.parse)
-    const newContent = data.filter(item => item.id !== contactId)
-    updateListContacts(newContent)
-  } catch (error) {
-    console.error(error)
+  async function removeContact(contactId) {
+    const data = await readContactsFile().then(JSON.parse);
+    const newContent = data.filter((item) => item.id !== contactId);
+    updateListContacts(newContent);
   }
-}
 
-async function addContact(name, email, phone) {
-  try {
-    const data = await readContactsFile().then(JSON.parse)
+  async function addContact(name, email, phone) {
+    const data = await readContactsFile().then(JSON.parse);
     const newContact = {
       id: shortid.generate(),
       name,
       email,
-      phone
-    }
-    updateListContacts([...data, newContact])
-  } catch (error) {
-    console.error(error)
+      phone,
+    };
+    updateListContacts([...data, newContact]);
   }
-}
 
-module.exports = {
+  module.exports = {
     listContacts,
     getContactById,
     removeContact,
-    addContact
+    addContact,
+  };
+} catch (error) {
+  console.error(error)
 }
